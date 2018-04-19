540da748bb35c71ab8f0dce44dc32d8809e071ea
christinekarimi <karimikim3@gmail.com>
2018-04-19 15:02:54 +0300

    Added a secure password function in the user class for authentication

--- app/models.py
+++ app/models.py
@@ -1,3 +1,4 @@
+from werkzeug.security import generate_password_hash, check_password_hash
 from . import db
 
 # userd class that will define all the different users
@@ -8,6 +9,23 @@ class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(250), index = True)
     email = db.Column(db.String(250), unique=True, index=True)
+    role_id = db.Column(db.Integer, db.ForeignKey('roles.id'))
+    pass_secure = db.Column(db.String(255))      #User authentication
+
+
+        @property
+        def password(self):
+            raise AttributeError('You cannot read the password attribute')
+
+        @password.setter
+        def password(self, password):
+            self.pass_secure = generate_password_hash(password)
+
+
+        def verify_password(self,password):
+            return check_password_hash(self.pass_secure,password)
+
+
 
 def __repr__(self):
         return f'User {self.username}'
