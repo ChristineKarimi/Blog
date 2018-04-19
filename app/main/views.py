2344638b53a96a207915b634138a317553b81509
christinekarimi <karimikim3@gmail.com>
2018-04-19 17:23:57 +0300

    Created a new table named blogs and carried out migrations

--- app/main/views.py
+++ app/main/views.py
@@ -1,9 +1,9 @@
 from flask import render_template, redirect, request, url_for, abort
 from flask_login import login_required, current_user
 from . import main
-from ..admin import admin
-from .forms import ReviewForm, BlogForm, EditBlog, DeleteBlog, DeleteComment
-from ..models import Blog, Review, User
+#from ..admin import admin
+#from .forms import ReviewForm, BlogForm, EditBlog, DeleteBlog, DeleteComment
+#from ..models import Blog, Review, User
 from .. import db
 
 #==============================================================================================================================================================================================================================
@@ -32,26 +32,4 @@ def new_review(id):
     return render_template('new_review.html', review_form=form, blog=blog)
 
 #===============================================================================================================================================================================================================================================
-
-@main.route('/blog/review/new/<int:id>', methods=['GET', 'POST'])
-def new_review(id):
-
-    blog = Blog.query.filter_by(id=id).first()
-
-    if blog is None:
-        abort(404)
-
-    form = ReviewForm()
-
-    if form.validate_on_submit():
-
-        review = form.review.data
-
-        new_review = Review(review=review, user_id=current_user.id, blog_id=blog.id)
-
-        new_review.save_review()
-
-        return redirect(url_for('.single_blog', id=blog.id))
-
-    return render_template('new_review.html', review_form=form, blog=blog)
 
--- app/models.py
+++ app/models.py
@@ -1,6 +1,8 @@
 from werkzeug.security import generate_password_hash, check_password_hash
 from . import db
 from flask_login import UserMixin
+from . import login_manager
+from datetime import datetime
 
 
 @login_manager.user_loader
@@ -22,16 +24,16 @@ class User(UserMixin, db.Model):
     pass_secure = db.Column(db.String(255))      #User authentication
     password_hash = db.Column(db.String(200))
 
-        @property
-        def password(self):
+@property
+def password(self):
             raise AttributeError('You cannot read the password attribute')
 
-        @password.setter
-        def password(self, password):
+@password.setter
+def password(self, password):
             self.pass_secure = generate_password_hash(password)
 
 
-        def verify_password(self,password):
+def verify_password(self,password):
             return check_password_hash(self.pass_secure,password)
 
 
@@ -51,7 +53,79 @@ class Role(db.Model):
     user = db.relationship('User', backref='role', lazy='dynamic')
 
 
-    def __repr__(self):
+def __repr__(self):
         return f'User {self.name}'
 
 #=================================================================================================================================================================================================
+
+
+class Review(db.Model):
+
+    __tablename__ = 'reviews'
+
+    id = db.Column(db.Integer, primary_key=True)
+    review = db.Column(db.String)
+    posted = db.Column(db.DateTime, default=datetime.now(tz=None))
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+    blog_id = db.Column(db.Integer, db.ForeignKey('blogs.id'))
+
+def save_review(self):
+        db.session.add(self)
+        db.session.commit()
+
+@classmethod
+def get_reviews(cls, id):
+        reviews = Review.query.filter_by(blog_id=id).all()
+        return reviews
+
+@classmethod
+def delete_review(self, review_id):
+
+        review = Review.query.filter_by(id=review_id).delete()
+
+        db.session.commit()
+
+#=======================================================================================================================================================================
+
+class Blog(db.Model):
+
+    __tablename__ = 'blogs'
+
+    id = db.Column(db.Integer, primary_key = True)
+
+    title = db.Column(db.String)
+
+    description = db.Column(db.String)
+
+    blog = db.Column(db.String)
+
+    posted = db.Column(db.DateTime, default=datetime.now(tz=None))
+
+    review_id = db.relationship('Review', backref='blog', lazy='dynamic')
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+
+def save_blog(self):
+        db.session.add(self)
+        db.session.commit()
+
+
+@classmethod
+def get_blog(cls, id):
+        blogs = Blog.query.order_by(Blog.posted.desc()).all()
+        return blogs
+
+@classmethod
+def delete_blog(self, blog_id):
+        reviews = Review.query.filter_by(blog_id=blog_id).delete()
+        blog = Blog.query.filter_by(id=blog_id).delete()
+        db.session.commit()
+
+@classmethod
+def edit_blog(self, blog_id):
+
+        blog = Review.query.filter_by(id=blog_id).edit()
+
+        db.session.commit()
+
+
+#============================================================================================================================================================================================
