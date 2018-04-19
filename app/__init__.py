362b2b659dab97713f4eceaba4a7dc8c5791d675
christinekarimi <karimikim3@gmail.com>
2018-04-19 15:29:03 +0300

    Added a route to enable authenticated users to add new reviews

--- app/__init__.py
+++ app/__init__.py
@@ -1,12 +1,20 @@
-
 from flask import Flask
-from config import config_options
 from flask_bootstrap import Bootstrap
+from config import config_options
 from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
+
 
 
 bootstrap = Bootstrap()
+
 db = SQLAlchemy()
+
+login_manager = LoginManager()
+login_manager.session_protection = 'strong'
+login_manager.login_view = 'auth.login'
+
+
 
 def create_app(config_state):
     app = Flask(__name__)
