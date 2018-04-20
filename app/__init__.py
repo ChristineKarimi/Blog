224725c419804c388499b5243703a7327af346a0
christinekarimi <karimikim3@gmail.com>
2018-04-20 09:42:39 +0300

    Registered the admin blueprint

--- app/__init__.py
+++ app/__init__.py
@@ -36,6 +36,10 @@ def create_app(config_name):
     from .auth import auth as auth_blueprint
     app.register_blueprint(auth_blueprint, url_prefix = '/authenticate')
 
+    # registering admin blueprint
+    from .admin import admin as admin_blueprint
+    app.register_blueprint(admin_blueprint, url_prefix = '/admin')
+
     
 
     return app
