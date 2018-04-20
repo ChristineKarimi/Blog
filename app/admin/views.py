9b679cb539a90ebb5c1d3e49252fc771e5f8319c
christinekarimi <karimikim3@gmail.com>
2018-04-20 09:54:26 +0300

    Added a dashboard template

--- app/admin/views.py
+++ app/admin/views.py
@@ -18,3 +18,23 @@ def check_user():
     return render_template('admin/dashboard.html')
 
 
+from flask import render_template, redirect, request, url_for, abort
+from flask_login import login_required, current_user
+
+from ..models import User
+from .forms import BlogForm, ReviewForm
+from . import admin
+from ..models import Blog, Review
+
+from .. import db
+
+
+def check_user():
+
+    if current_user.id != 1:
+
+        abort (403)
+
+    return render_template('admin/dashboard.html')
+
+
