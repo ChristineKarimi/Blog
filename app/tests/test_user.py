e629ce09aa8c01b4e6f98d54e6f9d6d165a08e8f
christinekarimi <karimikim3@gmail.com>
2018-04-23 07:59:40 +0300

    restructured folder structure

--- app/tests/test_user.py
+++ app/tests/test_user.py
@@ -1,5 +1,5 @@
-import unittest
-from app.models import User
+import unittest                       # Importing the unittest module
+from app.models import User            #import the user class
 
 class UserModelTest(unittest.TestCase):
 
@@ -9,11 +9,11 @@ class UserModelTest(unittest.TestCase):
     def test_password_setter(self):
         self.assertTrue(self.new_user.pass_secure is not None)
 
- def test_no_access_password(self):
+    def test_no_access_password(self):
             with self.assertRaises(AttributeError):
                 self.new_user.password
 
-        def test_password_verification(self):
+    def test_password_verification(self):
             self.assertTrue(self.new_user.verify_password('karimi'))
 
 #=============================================================================================================================================================================            
